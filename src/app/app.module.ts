1f49e288ec8b9dbe5a1b18e7e7b2332efde4e40c
ledroid <leandro.hdsl@gmail.com>
2016-11-05 18:56:30 -0500

    Diretivas Customizadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { DiretivasNgswitchComponent } from './diretivas-ngswitch/diretivas-ngswi
 import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
 import { OperadorElvisComponent } from './operador-elvis/operador-elvis.component';
+import { ExemploNgContentComponent } from './exemplo-ng-content/exemplo-ng-content.component';
+import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
+import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,11 @@ import { OperadorElvisComponent } from './operador-elvis/operador-elvis.componen
     DiretivasNgswitchComponent,
     DiretivaNgforComponent,
     DiretivaNgstyleComponent,
-    OperadorElvisComponent
+    OperadorElvisComponent,
+    ExemploNgContentComponent,
+    FundoAmareloDirective,
+    FundoAmareloDirective,
+    DiretivasCustomizadasComponent
   ],
   imports: [
     BrowserModule,
